0338889103b24331012049db788a46a79da58c6d
Peder Schacht <peteschacht@gmail.com>
2014-06-26 21:24:00 -0500

    api_request recieves json & tests filled out

--- app/models/api_request.rb
+++ app/models/api_request.rb
@@ -1,3 +1,4 @@
+require 'json'
 class ApiRequest < ActiveRecord::Base
   include HTTParty
   no_follow true
@@ -5,20 +6,20 @@ class ApiRequest < ActiveRecord::Base
     uri = ('https://us-prof.np.community.playstation.net/userProfile/v1/users/user_id/profile?fields=%40default,relation,requestMessageFlag,presence,%40personalDetail,trophySummary')
     uri.gsub!(/user_id/, user_id)
     response = ApiRequest.get(uri, :headers => @headers)
-    puts response
+    JSON.pretty_generate(response)
   end
   def self.get_trophies(user_id)
     uri = ('https://us-tpy.np.community.playstation.net/trophy/v1/trophyTitles?fields=%40default&npLanguage=en&platform=PS3,PSVITA,PS4&offset=0&limit=20&comparedUser=user_id')
     uri.gsub!(/user_id/, user_id)
     response = ApiRequest.get(uri, :headers => @headers)
-    puts response
+    JSON.pretty_generate(response)
   end
   def self.get_trophies_by_game(user_id, game_id)
     uri = ('https://us-tpy.np.community.playstation.net/trophy/v1/trophyTitles/game_id/trophyGroups/all/trophies?fields=%40default,trophyRare,trophyEarnedRate&npLanguage=en&comparedUser=user_id')
     uri.gsub!(/user_id/, user_id)
     uri.gsub!(/game_id/, game_id)
     response = ApiRequest.get(uri, :headers => @headers)
-    puts response
+    JSON.pretty_generate(response)
   end
   def self.get_trophy(user_id, game_id, trophy_id)
     uri = ('https://us-tpy.np.community.playstation.net/trophy/v1/trophyTitles/game_id/trophyGroups/all/trophies/trophy_id?fields=%40default,trophyRare,trophyEarnedRate&npLanguage=en&comparedUser=user_id')
@@ -26,7 +27,7 @@ class ApiRequest < ActiveRecord::Base
     uri.gsub!(/game_id/, game_id)
     uri.gsub!(/trophy_id/, trophy_id)
     response = ApiRequest.get(uri, :headers => @headers)
-    puts response
+    JSON.pretty_generate(response)
   end
   private
   tokens = UserToken.get_token
--- spec/models/api_request_spec.rb
+++ spec/models/api_request_spec.rb
@@ -4,21 +4,28 @@ RSpec.describe ApiRequest, :type => :model do
   describe "#get_profile"  do
     it "should return a user profile" do
       profile = ApiRequest.get_profile('studentnss')
+      expect(profile).to match(/"onlineId": "studentnss"/)
     end
   end
   describe "#get_trophies"  do
     it "should return a user's trophy list" do
       trophies = ApiRequest.get_trophies('studentnss')
+      expect(trophies).to match(/"trophyTitleName": "Diablo III"/)
     end
   end
   describe "#get_trophies_by_game"  do
     it "should return a games trophy list" do
-      games_trophies = ApiRequest.get_trophies_by_game('studentnss','NPWR04150_00')
+      game_trophies = ApiRequest.get_trophies_by_game('studentnss','NPWR04150_00')
+      expect(game_trophies).to match(/"trophyName": "Diablo III Platinum Trophy"/)
+      expect(game_trophies).to match(/"trophyId": 0/)
+      expect(game_trophies).to match(/"trophyId": 45/)
     end
   end
   describe "#get_trophy"  do
     it "should return a trophy" do
       trophy = ApiRequest.get_trophy('studentnss','NPWR04150_00','28')
+      expect(trophy).to match(/"trophyName": "Legends of the Brawl"/)
+      expect(trophy).to match(/"trophyId": 28/)
     end
   end
 end
